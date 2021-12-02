2b44c11dc681f27965709a6227c86add6cce029f
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-02 13:44:54 +0100

    fix not okay directory path

--- compare.sh
+++ compare.sh
@@ -1,3 +1,3 @@
 #usr/bin/bash
-difference="$(diff mandelbrot.ppm Mandelbrot_parallel.ppm)"
+difference="$(diff src/lib/a2/mandelbrot-original.ppm Mandelbrot_parallel.ppm)"
 [[ ! -z "$difference" ]] && echo "NOK" || echo "OK"
