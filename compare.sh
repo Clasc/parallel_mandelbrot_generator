f986f6e5cf0802c27d8b9012d9ad93379ab41773
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-03 22:16:39 +0100

    add taskloop solution to assertion

--- compare.sh
+++ compare.sh
@@ -1,3 +1,8 @@
 #usr/bin/bash
+echo "Asserting Tasks solution..."
 difference="$(diff src/lib/a2/mandelbrot-original.ppm Mandelbrot_parallel.ppm)"
+[[ ! -z "$difference" ]] && echo "NOK" || echo "OK"
+
+echo "Asserting Taskloops solution..."
+difference="$(diff src/lib/a2/mandelbrot-original.ppm Mandelbrot-taskloop.ppm)"
 [[ ! -z "$difference" ]] && echo "NOK" || echo "OK"
