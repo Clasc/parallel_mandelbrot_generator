0388e80c38d77b2a6cfa1de403f7d88a5c93abd6
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-03 22:24:37 +0100

    improve logging

--- src/a2-openmp.cpp
+++ src/a2-openmp.cpp
@@ -4,7 +4,9 @@ int main(int argc, char const* argv[]) {
     auto image = Image(3, 1024, 1536);
     auto tasks = TasksGenerator();
     auto taskloops = TaskloopGenerator();
+    std::cout << "Generating by Tasks..." << std::endl;
     tasks.generate(image);
+    std::cout << "Generating by Taskloop..." << std::endl;
     taskloops.generate(image, "Mandelbrot-taskloop.ppm");
     return 0;
 }
