7bdf826f54b014450a5975e6034d06383067b771
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-04 12:07:56 +0100

    add taskloop to 2d convolution

--- src/TasksGenerator.h
+++ src/TasksGenerator.h
@@ -217,6 +217,7 @@ public:
             {
                 for (int step = 0; step < nsteps; step++) {
                     for (int ch = 0; ch < channels; ch++) {
+                        #pragma omp taskloop
                         for (int i = 0; i < h; i++) {
                             for (int j = 0; j < w; j++) {
                                 double val = 0.0;
