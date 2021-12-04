1664a7e7c27ee28e404849a6689e399bb53102f1
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-04 12:06:15 +0100

    Assignment2: add prototype for taskloop

--- src/TasksGenerator.h
+++ src/TasksGenerator.h
@@ -171,35 +171,29 @@ public:
         vector<int> pixel = { 0, 0, 0 }; // red, green, blue (each range 0-255)
         complex<double> c;
 
-        #pragma omp parallel shared(image, ratio, pixels_inside) private (pixel, c)
+        #pragma omp parallel shared(image, ratio, pixels_inside) private(pixel, c)
         {
             #pragma omp single
             {
-                #pragma omp taskgroup
-                {
-                    for (int j = 0; j < h; j++) {
+                #pragma omp taskloop shared(pixels_inside) 
+                for (int j = 0; j < h; j++) {
+                    #pragma omp taskloop shared(pixels_inside) 
+                    for (int i = 0; i < w; i++) {
 
-                        #pragma omp task shared(w, h, channels, image, ratio, pixels_inside)
-                        {
-                            for (int i = 0; i < w; i++) {
+                        double dx = (double)i / (w)*ratio - 1.10;
+                        double dy = (double)j / (h) * 0.1 - 0.35;
 
-                                double dx = (double)i / (w)*ratio - 1.10;
-                                double dy = (double)j / (h) * 0.1 - 0.35;
+                        c = complex<double>(dx, dy);
 
-                                c = complex<double>(dx, dy);
-
-                                // the actual mandelbrot kernel
-                                if (mandelbrot_kernel(c, pixel)) {
-                                    #pragma omp atomic
-                                    pixels_inside++;
-                                }
-
-
-                                // apply to the image
-                                for (int ch = 0; ch < channels; ch++)
-                                    image(ch, j, i) = pixel[ch];
-                            }
+                        // the actual mandelbrot kernel
+                        if (mandelbrot_kernel(c, pixel)) {
+                            #pragma omp atomic
+                            pixels_inside++;
                         }
+
+                        // apply to the image
+                        for (int ch = 0; ch < channels; ch++)
+                            image(ch, j, i) = pixel[ch];
                     }
                 }
             }
@@ -222,38 +216,33 @@ public:
             #pragma omp single
             {
                 for (int step = 0; step < nsteps; step++) {
-                    #pragma omp taskgroup
-                    {
-                        for (int ch = 0; ch < channels; ch++) {
-                            for (int i = 0; i < h; i++) {
-                                #pragma omp task
-                                {
-                                    for (int j = 0; j < w; j++) {
-                                        double val = 0.0;
+                    for (int ch = 0; ch < channels; ch++) {
+                        for (int i = 0; i < h; i++) {
+                            for (int j = 0; j < w; j++) {
+                                double val = 0.0;
+                                for (int k = -displ; k <= displ; k++) {
+                                    for (int l = -displ; l <= displ; l++) {
+                                        int cy = i + k;
+                                        int cx = j + l;
+                                        int src_val = 0;
 
-                                        for (int k = -displ; k <= displ; k++) {
-                                            for (int l = -displ; l <= displ; l++) {
-                                                int cy = i + k;
-                                                int cx = j + l;
-                                                int src_val = 0;
-
-                                                // if it goes outside we disregard that value
-                                                if (cx < 0 || cx > w - 1 || cy < 0 || cy > h - 1) {
-                                                    continue;
-                                                }
-                                                else {
-                                                    src_val = src(ch, cy, cx);
-                                                }
-
-                                                val += kernel[k + displ][l + displ] * src_val;
-                                            }
+                                        // if it goes outside we disregard that value
+                                        if (cx < 0 || cx > w - 1 || cy < 0 || cy > h - 1) {
+                                            continue;
                                         }
-                                        dst(ch, i, j) = (int)(val > 255 ? 255 : (val < 0 ? 0 : val));
+                                        else {
+                                            src_val = src(ch, cy, cx);
+                                        }
+
+                                        val += kernel[k + displ][l + displ] * src_val;
                                     }
                                 }
+                                dst(ch, i, j) = (int)(val > 255 ? 255 : (val < 0 ? 0 : val));
                             }
+
                         }
                     }
+
 
                     if (step < nsteps - 1) {
                         // swap references
