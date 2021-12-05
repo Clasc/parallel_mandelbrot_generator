5c0df1068490e778985f61e7f449404575144d1e
Christian Lascsak <christian.lascsak7@gmail.com>
2021-12-05 18:43:55 +0100

    set threads to 16 and print it

--- src/a2-openmp.cpp
+++ src/a2-openmp.cpp
@@ -2,6 +2,14 @@
 
 int main(int argc, char const* argv[]) {
     auto image = Image(3, 1024, 1536);
+    omp_set_num_threads(16);
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            cout << "threads:" << omp_get_num_threads() << endl;
+        }
+    }
     auto tasks = TasksGenerator();
     auto taskloops = TaskloopGenerator();
 
